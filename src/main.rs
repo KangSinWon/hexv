5213002eefe02edb68ab250c3d1268a19c0d1610
KangSinWon <wnfansl788@gmail.com>
2023-11-16 13:43:01 +0900

    Change the data structure so that all files can be read

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use colored::*;
-use std::fs;
+use std::fs::File;
+use std::io::Read;
 
 /// Simple hex viewer
 #[derive(Parser, Debug)]
@@ -10,67 +11,83 @@ struct Cli {
     file: Option<String>,
 }
 
-fn run(f: &str) {
-    println!("\t   00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F | Decoded Text");
+struct Hexv {
+    buf: Vec<u8>,
+    idx: usize,
+    interval: usize,
+    line: usize,
+}
 
-    let mut index = 0;
-    let interval = 16;
-
-    let mut line = 0;
+fn run(mut hexv: Hexv) {
     loop {
-        if index > f.len() {
+        if hexv.idx >= hexv.buf.len() {
             break;
         }
 
-        let s: &str;
-        if index + interval > f.len() {
-            s = &f[index..];
+        let buf_slice: Vec<u8>;
+        if hexv.idx + hexv.interval > hexv.buf.len() {
+            buf_slice = hexv.buf[hexv.idx..].to_vec();
         } else {
-            s = &f[index..(index + interval)];
+            buf_slice = hexv.buf[hexv.idx..(hexv.idx + hexv.interval)].to_vec();
         }
 
-        let chars: Vec<char> = s.chars().collect();
+        let mut hex_len = 0;
         let mut hex_str = String::from("");
-        for t in chars {
-            hex_str += &format!("{:02x} ", t as i32);
+        let mut dec_str = String::from("");
+        for &t in &buf_slice {
+            if t == 0 {
+                dec_str += &"0".black().to_string();
+            } else if t > 9 && t < 14 {
+                dec_str += &"\\".red().to_string();
+            } else if t < 32 || t > 126 {
+                dec_str += &"x".yellow().to_string();
+            } else {
+                dec_str += &(t as char).to_string().blue().to_string();
+            }
+
+            hex_str += &format!("{:02x} ", t as i32).green().to_string();
+            hex_len += 3;
         }
-
-        if hex_str.len() < interval * 3 {}
-        hex_str += &format!(" ").repeat((interval * 3) - hex_str.len());
-
-        let dec: String = s
-            .chars()
-            .map(|c| {
-                if c == '\r' || c == '\n' {
-                    "\\".red().to_string()
-                } else {
-                    c.to_string().blue().to_string()
-                }
-            }).collect();
+        
+        if (hexv.interval * 3) - hex_len > 0 {
+            hex_str += &format!(" ").repeat((hexv.interval * 3) - hex_len);
+        }
 
         println!(
-            "0x{:08} {}| {}",
-            line,
-            hex_str.green(),
-            dec
-            // s.replace("\r", "\\").replace("\n", "\\").blue()
+            "0x{:08x} {}| {}",
+            hexv.line,
+            hex_str,
+            dec_str
         );
 
-        index += interval;
-        line += 10;
+        hexv.idx += hexv.interval;
+        hexv.line += hexv.interval;
     }
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let cli = Cli::parse();
 
     if let Some(file_name) = cli.file.as_deref() {
-        let f = fs::read_to_string(file_name).expect("Should have been able to read the file");
-
         println!("hexv: {}", file_name);
-        run(f.as_str());
+
+        let mut f = File::open(file_name)?;
+        let mut buf = Vec::new();
+        f.read_to_end(&mut buf)?;
+
+        println!("\t   00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F | Decoded Text");
+
+        let hexv = Hexv {
+            buf,
+            idx: 0,
+            interval: 16,
+            line: 0,
+        };
+
+        run(hexv);
     } else {
         println!("Please enter the file");
-        std::process::exit(0);
     }
+
+    Ok(())
 }
